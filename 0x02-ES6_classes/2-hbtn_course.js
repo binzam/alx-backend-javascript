0db5168518213fcde17a3bf94468a47ea5121859
root <btechan@gmail.com>
2024-04-11 20:45:07 +0000

    tasks

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,63 +1,49 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
+
   constructor(name, length, students) {
-    this._name = this._validateString(name, 'name');
-    this._length = this._validateNumber(length, 'length');
-    this._students = this._validateArray(students, 'students');
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  // Getter for name
+
   get name() {
     return this._name;
   }
 
-  // Setter for name
-  set name(newName) {
-    this._name = this._validateString(newName, 'name');
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
   }
 
-  // Getter for length
+
   get length() {
     return this._length;
   }
 
-  // Setter for length
-  set length(newLength) {
-    this._length = this._validateNumber(newLength, 'length');
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
   }
 
-  // Getter for students
+
   get students() {
     return this._students;
   }
 
-  // Setter for students
-  set students(newStudents) {
-    this._students = this._validateArray(newStudents, 'students');
-  }
 
-  // Helper method to validate string type
-  _validateString(value, attribute) {
-    if (typeof value !== 'string') {
-      throw new Error(`${attribute} must be a string.`);
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
     }
-    return value;
-  }
-
-  // Helper method to validate number type
-  _validateNumber(value, attribute) {
-    if (typeof value !== 'number') {
-      throw new Error(`${attribute} must be a number.`);
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
-    return value;
-  }
-
-  // Helper method to validate array type
-  _validateArray(value, attribute) {
-    if (!Array.isArray(value)) {
-      throw new Error(`${attribute} must be an array.`);
-    }
-    return value;
+    this._students = value;
   }
 }
-
-export default HolbertonCourse;
