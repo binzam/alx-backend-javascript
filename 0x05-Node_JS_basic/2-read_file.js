52a62dc43628a5a80edc0c6dd5b7214c1500ce31
binzam <btechan@gmail.com>
2024-06-04 02:23:49 +0000

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  const db = fs
+    .readFileSync(path, 'utf-8')
+    .toString('utf-8')
+    .trim()
+    .split('\n');
+  if (!fs.existsSync(path) || !db) {
+    throw new Error('Cannot load the database');
+  }
+  const dbHeaders = db[0].split(',');
+  const students = db.slice(1);
+  const studentObjects = students.map((student) => {
+    const values = student.split(',');
+    const studentObj = {};
+    dbHeaders.forEach((header, index) => {
+      studentObj[header] = values[index];
+    });
+    return studentObj;
+  });
+  const totalStudents = studentObjects.length;
+  console.log(`Number of students: ${totalStudents}`);
+  const fieldCount = {};
+  const fieldStudents = {};
+  studentObjects.forEach((student) => {
+    const { field } = student;
+    if (fieldCount[field]) {
+      fieldCount[field] += 1;
+      fieldStudents[field].push(student.firstname);
+    } else {
+      fieldCount[field] = 1;
+      fieldStudents[field] = [student.firstname];
+    }
+  });
+
+  Object.keys(fieldCount).forEach((field) => {
+    console.log(
+      `Number of students in ${field}: ${
+        fieldCount[field]
+      }. List: ${fieldStudents[field].join(', ')}`,
+    );
+  });
+}
+
+module.exports = countStudents;
