a2ec55cb67e7b09b2547082d780998ddbdb03c44
binzam <btechan@gmail.com>
2024-06-04 23:51:41 +0000

    task 6

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const dbFile = process.argv[2];
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (req.url === '/students') {
+    countStudents(dbFile)
+      .then((data) => {
+        res.write('This is the list of our students\n');
+        res.write(`Number of students: ${data.totalStudents}\n`);
+        Object.keys(data.fieldCount).forEach((field) => {
+          res.write(
+            `Number of students in ${field}: ${
+              data.fieldCount[field]
+            }. List: ${data.fieldStudents[field].join(', ')}\n`,
+          );
+        });
+        res.end();
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write(`Error: ${error.message}`);
+        res.end();
+      });
+  }
+});
+app.listen('1245');
+
+module.exports = app;
