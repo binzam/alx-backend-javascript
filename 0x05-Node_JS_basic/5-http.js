7220b4f2c0c2b76b6f62908fdd08bd7f0189e86f
binzam <btechan@gmail.com>
2024-06-05 23:46:03 +0000

    task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const countStudents = require('./helper');
 
-const dbFile = process.argv[2];
+const dbFile = process.argv[2].toString();
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -10,7 +10,7 @@ const app = http.createServer((req, res) => {
     res.end();
   }
   if (req.url === '/students') {
-    res.write('This is the list of our students');
+    res.write('This is the list of our students\n');
     countStudents(dbFile)
       .then((data) => {
         res.write(data.output.slice(0, -1));
