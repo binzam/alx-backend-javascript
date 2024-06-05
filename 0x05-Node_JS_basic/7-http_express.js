98151c888d84e2ee4d01f67ee6591c48e966734a
binzam <btechan@gmail.com>
2024-06-05 23:57:05 +0000

    task 5

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,28 +1,26 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const countStudents = require('./helper');
 
 const dbFile = process.argv[2];
 const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.type('text').send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
   countStudents(dbFile)
     .then((data) => {
-      let response = 'This is the list of our students\n';
-      response += `Number of students: ${data.totalStudents}\n`;
-      Object.keys(data.fieldCount).forEach((field) => {
-        response += `Number of students in ${field}: ${
-          data.fieldCount[field]
-        }. List: ${data.fieldStudents[field].join(', ')}\n`;
-      });
-      res.type('text').send(response);
+      res.send(
+        ['This is the list of our students', data.output.slice(0, -1)].join(
+          '\n',
+        ),
+      );
+      res.end();
     })
-    .catch((error) => {
-      res.status(500).send(`Error: ${error.message}`);
+    .catch(() => {
+      res.send('This is the list of our students\nCannot load the database');
     });
 });
 
@@ -31,3 +29,4 @@ app.listen(port, () => {
 });
 
 module.exports = app;
+
