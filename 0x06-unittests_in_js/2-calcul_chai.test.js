b94135b536fa7f2ef74bce2f6687ac61227e8a09
binzam <btechan@gmail.com>
2024-06-07 02:04:41 +0300

    task 2

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,20 @@
+const expect = require("chai").expect;
+const calculateNumber = require("./2-calcul_chai");
+
+describe('calculateNumber', function () {
+  it('should return the correct sum of rounded numbers when type is SUM', function () {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+
+  it('should return the correct result when type is SUBTRACT', function () {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+
+  it('should handle division and return the correct result when type is DIVIDE', function () {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+
+  it('should handle division by 0 and return "Error" when type is DIVIDE', function () {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+});
