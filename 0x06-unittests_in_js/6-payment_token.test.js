4e616348b7d227b0fc430a0037458007fabfd8a4
binzam <btechan@gmail.com>
2024-06-07 02:58:13 +0300

    task 6

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function () {
+  it('should return a resolved promise with the correct data when success is true', function (done) {
+    getPaymentTokenFromAPI(true).then((response) => {
+      expect(response).to.have.property('data');
+    });
+    done();
+  });
+});
