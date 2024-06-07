e627928ee6ee3fa1abda21bb591fa1c59c5fa32a
binzam <btechan@gmail.com>
2024-06-07 03:03:27 +0300

    task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,7 +5,7 @@ describe('getPaymentTokenFromAPI', function () {
   it('should return a resolved promise with the correct data when success is true', function (done) {
     getPaymentTokenFromAPI(true).then((response) => {
       expect(response).to.have.property('data');
+      done();
     });
-    done();
   });
 });
